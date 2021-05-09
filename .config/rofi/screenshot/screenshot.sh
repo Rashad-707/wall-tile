3b53b05ca8f4fe496f8b76e9ea9b18c36e727098
RaShaD_707 <61148691+Rashad-707@users.noreply.github.com>
2021-05-10 01:30:56 +0300

    Create screenshot.sh

---
+++ .config/rofi/screenshot/screenshot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+# Bind this script with a keyboard shourtcut (eg. in i3 config file)
+
+screen=""
+area=""
+window=""
+
+chosen=$(printf '%s;%s;%s\n' "$screen" "$area" "$window" \
+    | rofi -theme 'screenshot/screenshot.rasi' \
+           -dmenu \
+           -sep ';' \
+           -selected-row 0)
+
+case "$chosen" in
+    "$screen")
+        spectacle -f -b
+    ;;
+
+    "$area")
+        spectacle -r -b
+    ;;
+
+    "$window")
+        spectacle -u -b
+    ;;
+
+    *) exit 1 ;;
+esac
